19f35691d54317f05e8fcc13823c0338d6e9d2bc
甲甲 <51043917+jiajiaxd@users.noreply.github.com>
2022-03-18 09:19:42 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -3,3 +3,6 @@ rm -rf .github
 rm -rf .gitignore
 mkdir .minecraft
 mv * .minecraft
+git clone https://github.com/Project-StarryNight/snp_paperbox_patch.git patch_file
+mv -f patch_file/* ./
+rm -rf patch_file
